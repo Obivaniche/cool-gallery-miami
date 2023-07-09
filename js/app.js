f2e7829de040f530f7c4feb2130d6ba423afbc28
Vladialsv Smirnov <obivaniche@yandex.ru>
2023-07-09 06:51:04 +0300

    Deploy

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const sliderMain = new Swiper('.slider_main', {
+	freeMode: true,
+	centeredSlides: true,
+	mousewheel: true,
+	parallax: true,
+	breakpoints: {
+		0: {
+			slidesPerView: 1.5,
+			spaceBetween: 20
+		},
+		480: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		},
+		720: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
+});
+
+const sliderBg = new Swiper('.slider_bg', {
+	centeredSlides: true,
+	spaceBetween: 60,
+	parallax: true,
+	slidesPerView: 3.5
+});
+
+sliderMain.controller.control = sliderBg;
+
+document.querySelectorAll('.slider__item').forEach(item => {
+	item.addEventListener('click', event => {
+		item.classList.toggle('opened')
+	})
+});
+
+let desc = document.querySelector('.description');
+
+sliderMain.on('slideChange', e => {
+	sliderMain.activeIndex > 0
+		? desc.classList.add('hidden')
+		: desc.classList.remove('hidden')
+});

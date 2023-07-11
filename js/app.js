a91a3b19387808250fb3949cb25f4e9e51333e21
Vladialsv Smirnov <obivaniche@yandex.ru>
2023-07-11 15:58:41 +0300

    Add audio

--- js/app.js
+++ js/app.js
@@ -48,3 +48,19 @@ sliderMain.on('slideChange', e => {
 		? desc.classList.add('hidden')
 		: desc.classList.remove('hidden')
 });
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
